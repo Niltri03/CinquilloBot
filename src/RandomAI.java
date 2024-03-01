abb697ac17935a21eb4803e5dc30f6548a8ea322
Niltri03 <nachofobo@gmail.com>
2024-03-01 16:16:23 +0100

    test git local

--- src/RandomAI.java
+++ src/RandomAI.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Random;
+
 
 public class RandomAI implements AI{
+    Random seed = new Random();
     public Carta juegaCarta(ArrayList<Carta> mano, Partida p){
-
+        return mano.get(seed.nextInt(mano.size()));
     }
 
 }
